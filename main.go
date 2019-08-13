3ff5bdacb52457f33130978749e0f1f9c21798d0
tslearn <tslearn@163.com>
2019-08-13 09:20:51 +0800

    commit

--- main.go
+++ main.go
@@ -2,25 +2,58 @@ package main
 
 import (
 	"fmt"
-	"github.com/huandu/go-tls/g"
 	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/tslearn/rpcc/g"
 )
 
 var gmap = sync.Map{}
 var count = uint64(0)
 
+const loop = 1000000
+
+func testEE() (ret []byte) {
+	ret = make([]byte, 800, 800)
+	for i := 0; i < 800; i++ {
+		ret[i] = 12
+	}
+	return
+}
+
+func testDD() (ret []byte) {
+	ret = testEE()
+
+	if len(ret) == 8 {
+		for i := 0; i < 8; i++ {
+			if ret[i] != 12 {
+				fmt.Println("NO")
+			}
+		}
+	}
+	return ret
+}
+
+func testCC() (ret []byte) {
+	ret = testDD()
+	if len(ret) == 3 {
+		fmt.Println("NO")
+	}
+	return
+}
+
 func routine(idx int) {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
+		testCC()
 		ptr, ok := gmap.Load(idx)
 		if ok {
 			if ptr.(uintptr) != uintptr(g.G()) {
 				fmt.Println("Error")
 			} else {
-				if v := atomic.AddUint64(&count, 1); v%1000000 == 0 {
-					fmt.Println("OK", count)
+				if v := atomic.AddUint64(&count, 1); v%loop == 0 {
+					fmt.Println("OK", v)
 				}
 			}
 		} else {
@@ -31,17 +64,13 @@ func routine(idx int) {
 }
 
 func main() {
-	mu := sync.Mutex{}
-
 	runtime.GC()
 
-	mu.Lock()
-
-	for n := 0; n < 1; n++ {
+	for n := 0; n < loop; n++ {
 		go routine(n)
 	}
 
-	time.Sleep(100 * time.Second)
-
-	mu.Unlock()
+	for {
+		time.Sleep(time.Millisecond)
+	}
 }
