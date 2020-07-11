a74728cc824b846ba88a6ea772adc40e9defd903
tslearn <tslearn@163.com>
2020-07-11 12:27:06 +0800

    commit

--- util/assert_test.go
+++ util/assert_test.go
@@ -18,7 +18,7 @@ func TestIsNil(t *testing.T) {
 }
 
 // test IsNil fail
-func runReportAssertFail(fn func(assert func(args ...interface{}) *rpcAssert)) {
+func runWithFail(fn func(assert func(args ...interface{}) *rpcAssert)) bool {
 	ch := make(chan bool, 1)
 	fn(func(args ...interface{}) *rpcAssert {
 		return &rpcAssert{
@@ -29,7 +29,13 @@ func runReportAssertFail(fn func(assert func(args ...interface{}) *rpcAssert)) {
 			args: args,
 		}
 	})
-	<-ch
+
+	select {
+	case <-ch:
+		return true
+	default:
+		return false
+	}
 }
 
 func TestNewAssert(t *testing.T) {
@@ -43,9 +49,10 @@ func TestNewAssert(t *testing.T) {
 }
 
 func TestAssert_Fail(t *testing.T) {
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert := NewAssert(t)
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(nil).Fail()
-	})
+	})).IsTrue()
 }
 
 func TestAssert_Equals(t *testing.T) {
@@ -60,31 +67,31 @@ func TestAssert_Equals(t *testing.T) {
 	assert(map[int]string{3: "OK", 4: "NO"}).
 		Equals(map[int]string{4: "NO", 3: "OK"})
 
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(nil).Equals(0)
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(3).Equals(uint(3))
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert((interface{})(nil)).Equals(0)
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert([]int{1, 2, 3}).Equals([]int64{1, 2, 3})
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert([]int{1, 2, 3}).Equals([]int32{1, 2, 3})
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(map[int]string{3: "OK", 4: "NO"}).
 			Equals(map[int64]string{4: "NO", 3: "OK"})
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert().Equals(3)
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(3, 3).Equals(3)
-	})
+	})).IsTrue()
 }
 
 func TestAssert_IsNil(t *testing.T) {
@@ -94,78 +101,78 @@ func TestAssert_IsNil(t *testing.T) {
 	assert((*rpcAssert)(nil)).IsNil()
 	assert((unsafe.Pointer)(nil)).IsNil()
 
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(uintptr(0)).IsNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(NewAssert(t)).IsNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(32).IsNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(false).IsNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(0).IsNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert().IsNil()
-	})
+	})).IsTrue()
 }
 
 func TestAssert_IsNotNil(t *testing.T) {
 	assert := NewAssert(t)
 	assert(t).IsNotNil()
 
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(nil).IsNotNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert((*rpcAssert)(nil)).IsNotNil()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert().IsNotNil()
-	})
+	})).IsTrue()
 }
 
 func TestAssert_IsTrue(t *testing.T) {
 	assert := NewAssert(t)
 	assert(true).IsTrue()
 
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert((*rpcAssert)(nil)).IsTrue()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(32).IsTrue()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(false).IsTrue()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(0).IsTrue()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert().IsTrue()
-	})
+	})).IsTrue()
 }
 
 func TestAssert_IsFalse(t *testing.T) {
 	assert := NewAssert(t)
 	assert(false).IsFalse()
 
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(32).IsFalse()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(true).IsFalse()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert(0).IsFalse()
-	})
-	runReportAssertFail(func(assert func(args ...interface{}) *rpcAssert) {
+	})).IsTrue()
+	assert(runWithFail(func(assert func(args ...interface{}) *rpcAssert) {
 		assert().IsFalse()
-	})
+	})).IsTrue()
 }
 
 type testAssertFail struct {
