a8cd7c6dfb507cbc82b0ddaa23e26e89cb6cd38a
tslearn <tslearn@163.com>
2020-07-11 23:36:23 +0800

    change

--- util/rand.go
+++ util/rand.go
@@ -2,28 +2,15 @@ package util
 
 import (
 	"math/rand"
-	"sync/atomic"
 	"time"
 )
 
 const (
-	randCacheSize = 1000001
-	base64String  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+	base64String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
 		"abcdefghijklmnopqrstuvwxyz" +
 		"0123456789" +
 		"+/"
 )
-
-var (
-	randPos         = uint64(0)
-	randCacheUint32 = make([]uint32, randCacheSize, randCacheSize)
-)
-
-// GetRandUint32 get Uint32 rand and fast,
-// this is approximate rand but high performance
-func GetRandUint32() uint32 {
-	return randCacheUint32[atomic.AddUint64(&randPos, 1)%randCacheSize]
-}
 
 // GetRandString get random string
 func GetRandString(strLen int) string {
@@ -43,7 +30,4 @@ func GetRandString(strLen int) string {
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < randCacheSize; i++ {
-		randCacheUint32[i] = rand.Uint32()
-	}
 }
--- util/rand_test.go
+++ util/rand_test.go
@@ -4,16 +4,6 @@ import (
 	"testing"
 )
 
-func TestGetRandUint32(t *testing.T) {
-	assert := NewAssert(t)
-	sum := int64(0)
-	for i := 0; i < 100000; i++ {
-		sum += int64(GetRandUint32())
-	}
-	delta := sum/100000 - 2147483648
-	assert(delta > -20000000 && delta < 20000000).IsTrue()
-}
-
 func TestGetRandString(t *testing.T) {
 	assert := NewAssert(t)
 	assert(GetRandString(-1)).Equals("")
@@ -22,7 +12,7 @@ func TestGetRandString(t *testing.T) {
 	}
 }
 
-func BenchmarkGetRandUint32(b *testing.B) {
+func BenchmarkGetRandString(b *testing.B) {
 	b.ReportAllocs()
 	for n := 0; n < b.N; n++ {
 		GetRandString(128)
