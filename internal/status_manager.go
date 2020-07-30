08903852bf4f20096e60e1b4e06c851104479070
tslearn <tslearn@163.com>
2020-07-31 01:16:42 +0800

    change

--- internal/status_manager.go
+++ internal/status_manager.go
@@ -12,76 +12,58 @@ type StatusManager struct {
 	lock    Lock
 }
 
-func (p *StatusManager) SetRunning(onSuccess func(), onFail func()) bool {
+func (p *StatusManager) SetRunning(onSuccess func()) bool {
 	return p.lock.CallWithLock(func() interface{} {
-		ret := atomic.CompareAndSwapInt32(
+		if atomic.CompareAndSwapInt32(
 			&p.status,
 			statusManagerClosed,
 			statusManagerRunning,
-		)
-
-		if ret {
+		) {
 			if onSuccess != nil {
 				onSuccess()
 			}
-		} else {
-			if onFail != nil {
-				onFail()
-			}
+			return true
 		}
 
-		return ret
+		return false
 	}).(bool)
 }
 
-func (p *StatusManager) SetClosing(
-	onSuccess func(ch chan struct{}),
-	onFail func(),
-) bool {
+func (p *StatusManager) SetClosing(onSuccess func(ch chan struct{})) bool {
 	return p.lock.CallWithLock(func() interface{} {
-		ret := atomic.CompareAndSwapInt32(
+		if atomic.CompareAndSwapInt32(
 			&p.status,
 			statusManagerRunning,
 			statusManagerClosing,
-		)
-
-		if ret {
+		) {
 			p.closeCH = make(chan struct{}, 1)
 			if onSuccess != nil {
 				onSuccess(p.closeCH)
 			}
-		} else {
-			if onFail != nil {
-				onFail()
-			}
+			return true
 		}
 
-		return ret
+		return false
 	}).(bool)
 }
 
-func (p *StatusManager) SetClosed(onSuccess func(), onFail func()) bool {
+func (p *StatusManager) SetClosed(onSuccess func()) bool {
 	return p.lock.CallWithLock(func() interface{} {
-		ret := atomic.CompareAndSwapInt32(
+		if atomic.CompareAndSwapInt32(
 			&p.status,
 			statusManagerClosing,
 			statusManagerClosed,
-		)
-
-		if ret {
+		) {
 			if onSuccess != nil {
 				onSuccess()
 			}
 			p.closeCH <- struct{}{}
 			close(p.closeCH)
 			p.closeCH = nil
-		} else {
-			if onFail != nil {
-				onFail()
-			}
+			return true
 		}
 
-		return ret
+		return false
 	}).(bool)
 }
 
