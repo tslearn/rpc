616288b7a99748bad2ddf4ec8bab4c34987ef9e9
tslearn <tslearn@163.com>
2020-08-06 13:33:01 +0800

    change

--- server_core_test.go
+++ server_core_test.go
@@ -31,7 +31,7 @@ func TestServer_Basic(t *testing.T) {
 		panic(err)
 	}
 
-	for i := 0; i < 50000; i++ {
+	for i := 0; i < 500; i++ {
 		go func(idx int) {
 			fmt.Println(client.sendMessage(
 				5*time.Second,
