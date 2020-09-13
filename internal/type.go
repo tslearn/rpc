a99c41ce3031f9be8442482031b976c112aaaf5d
tslearn <tslearn@163.com>
2020-09-13 15:56:33 +0800

    fast dev

--- internal/type.go
+++ internal/type.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"net"
+	"reflect"
+	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 // IStreamConn ...
@@ -68,3 +71,272 @@ type returnObject struct{}
 type Return = *returnObject
 
 var emptyReturn = &returnObject{}
+
+type RTArray struct {
+	rt    Runtime
+	items []posRecord
+}
+
+func newRTArray(rt Runtime, size int) (ret RTArray) {
+	ret.rt = rt
+
+	if thread := rt.thread; thread != nil {
+		if data := thread.malloc(sizeOfPosRecord * size); data != nil {
+			itemsHeader := (*reflect.SliceHeader)(unsafe.Pointer(&ret.items))
+			itemsHeader.Len = 0
+			itemsHeader.Cap = size
+			itemsHeader.Data = uintptr(data)
+			return
+		}
+	}
+
+	ret.items = make([]posRecord, 0, size)
+	return
+}
+
+func (p *RTArray) Get(index int) RTValue {
+	if index >= 0 && index < len(p.items) {
+		return makeRTValue(p.rt, p.items[index])
+	} else {
+		return RTValue{}
+	}
+}
+
+func (p *RTArray) Size() int {
+	if p.items != nil {
+		return len(p.items)
+	} else {
+		return -1
+	}
+}
+
+type mapItem struct {
+	key string
+	pos posRecord
+}
+
+const sizeOfMapItem = int(unsafe.Sizeof(mapItem{}))
+
+type RTMap struct {
+	rt       Runtime
+	items    []mapItem
+	largeMap map[string]posRecord
+}
+
+func newRTMap(rt Runtime, size int) (ret RTMap) {
+	ret.rt = rt
+
+	if thread := rt.thread; thread != nil && size <= 8 {
+		if data := thread.malloc(sizeOfMapItem * size); data != nil {
+			itemsHeader := (*reflect.SliceHeader)(unsafe.Pointer(&ret.items))
+			itemsHeader.Len = 0
+			itemsHeader.Cap = size
+			itemsHeader.Data = uintptr(data)
+			ret.largeMap = nil
+			return
+		}
+	}
+
+	if size <= 16 {
+		ret.items = make([]mapItem, 0, size)
+		ret.largeMap = nil
+	} else {
+		ret.items = nil
+		ret.largeMap = make(map[string]posRecord)
+	}
+
+	return
+}
+
+func (p *RTMap) Get(key string) RTValue {
+	if p.items != nil {
+		for i := len(p.items) - 1; i >= 0; i-- {
+			if key == p.items[i].key {
+				return makeRTValue(p.rt, p.items[i].pos)
+			}
+		}
+		return RTValue{}
+	} else if p.largeMap != nil {
+		if pos, ok := p.largeMap[key]; ok {
+			return makeRTValue(p.rt, pos)
+		}
+		return RTValue{}
+	} else {
+		return RTValue{}
+	}
+}
+
+func (p *RTMap) Size() int {
+	if p.items != nil {
+		return len(p.items)
+	} else if p.largeMap != nil {
+		return len(p.largeMap)
+	} else {
+		return -1
+	}
+}
+
+func (p *RTMap) appendValue(key string, pos posRecord) {
+	if p.items != nil {
+		p.items = append(p.items, mapItem{key: key, pos: pos})
+	} else {
+		p.largeMap[key] = pos
+	}
+}
+
+type posRecord uint64
+
+const sizeOfPosRecord = int(unsafe.Sizeof(posRecord(0)))
+
+func (p posRecord) getPos() int64 {
+	return int64(p) & 0x7FFFFFFFFFFFFFFF
+}
+
+func (p posRecord) isString() bool {
+	return (p & 0x8000000000000000) != 0
+}
+
+func makePosRecord(pos int64, isString bool) posRecord {
+	if !isString {
+		return posRecord(pos)
+	} else {
+		return 0x8000000000000000 | posRecord(pos)
+	}
+}
+
+type RTValue struct {
+	rt          Runtime
+	pos         int64
+	cacheString string
+	cacheOK     bool
+}
+
+func makeRTValue(rt Runtime, record posRecord) RTValue {
+	if !record.isString() {
+		return RTValue{
+			rt:          rt,
+			pos:         record.getPos(),
+			cacheString: "",
+			cacheOK:     false,
+		}
+	} else if thread := rt.lock(); thread == nil {
+		return RTValue{
+			rt:          rt,
+			pos:         record.getPos(),
+			cacheString: "",
+			cacheOK:     false,
+		}
+	} else {
+		defer rt.unlock()
+		pos := record.getPos()
+		thread.rtStream.SetReadPos(int(pos))
+		ret := RTValue{
+			rt:  rt,
+			pos: record.getPos(),
+		}
+		ret.cacheString, ret.cacheOK = thread.rtStream.ReadUnsafeString()
+		return ret
+	}
+}
+
+func (p RTValue) ToBool() (Bool, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadBool()
+	}
+
+	return false, false
+}
+
+func (p RTValue) ToInt64() (Int64, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadInt64()
+	}
+
+	return 0, false
+}
+
+func (p RTValue) ToUint64() (Uint64, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadUint64()
+	}
+
+	return 0, false
+}
+
+func (p RTValue) ToFloat64() (Float64, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadFloat64()
+	}
+
+	return 0, false
+}
+
+func (p RTValue) ToString() (String, bool) {
+	buf := []byte(p.cacheString)
+	ret := string(buf)
+
+	if p.cacheOK &&
+		atomic.LoadUint64(&p.rt.thread.top.lockStatus) == p.rt.id {
+		return ret, true
+	} else {
+		return "", false
+	}
+}
+
+func (p RTValue) ToBytes() (Bytes, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadBytes()
+	}
+
+	return Bytes(nil), false
+}
+
+func (p RTValue) ToArray() (Array, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadArray()
+	}
+
+	return Array(nil), false
+}
+
+func (p RTValue) ToRTArray() (RTArray, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadRTArray(p.rt)
+	}
+
+	return RTArray{}, false
+}
+
+func (p RTValue) ToMap() (Map, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadMap()
+	}
+
+	return Map(nil), false
+}
+
+func (p RTValue) ToRTMap() (RTMap, bool) {
+	if thread := p.rt.lock(); thread != nil {
+		defer p.rt.unlock()
+		thread.rtStream.SetReadPos(int(p.pos))
+		return thread.rtStream.ReadRTMap(p.rt)
+	}
+
+	return RTMap{}, false
+}
