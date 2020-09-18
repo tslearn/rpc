3f62d1b09285a68c1bed1a4a672115c33d42a39c
tslearn <tslearn@163.com>
2020-09-18 20:31:03 +0800

    fast dev

--- internal/base/util_test.go
+++ internal/base/util_test.go
@@ -2,9 +2,7 @@ package base
 
 import (
 	"strings"
-	"sync/atomic"
 	"testing"
-	"time"
 	"unsafe"
 )
 
@@ -59,113 +57,6 @@ func TestIsUTF8Bytes(t *testing.T) {
 	assert(IsUTF8Bytes([]byte{0xF1, 0x80, 0x80, 0x70})).IsFalse()
 
 	assert(IsUTF8Bytes([]byte{0xFF, 0x80, 0x80, 0x70})).IsFalse()
-}
-
-func TestConvertToIsoDateString(t *testing.T) {
-	assert := NewAssert(t)
-	start, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"0001-01-01T00:00:00+00:00",
-	)
-
-	for i := 0; i < 1000000; i++ {
-		parseTime, err := time.Parse(
-			"2006-01-02T15:04:05.999Z07:00",
-			ConvertToIsoDateString(start),
-		)
-		assert(err).IsNil()
-		assert(parseTime.UnixNano()).Equal(start.UnixNano())
-		start = start.Add(271099197000000)
-	}
-
-	smallTime, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"0000-01-01T00:00:00+00:00",
-	)
-	assert(ConvertToIsoDateString(smallTime)).
-		Equal("0000-01-01T00:00:00.000+00:00")
-
-	largeTime, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"9998-01-01T00:00:00+00:00",
-	)
-	largeTime = largeTime.Add(1000000 * time.Hour)
-	assert(ConvertToIsoDateString(largeTime)).
-		Equal("9999-01-30T16:00:00.000+00:00")
-
-	time1, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"2222-12-22T11:11:11.333-11:59",
-	)
-	assert(ConvertToIsoDateString(time1)).
-		Equal("2222-12-22T11:11:11.333-11:59")
-
-	time2, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"2222-12-22T11:11:11.333+11:59",
-	)
-	assert(ConvertToIsoDateString(time2)).
-		Equal("2222-12-22T11:11:11.333+11:59")
-
-	time3, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"2222-12-22T11:11:11.333+00:00",
-	)
-	assert(ConvertToIsoDateString(time3)).
-		Equal("2222-12-22T11:11:11.333+00:00")
-
-	time4, _ := time.Parse(
-		"2006-01-02T15:04:05.999Z07:00",
-		"2222-12-22T11:11:11.333-00:00",
-	)
-	assert(ConvertToIsoDateString(time4)).
-		Equal("2222-12-22T11:11:11.333+00:00")
-}
-
-func TestTimeNow(t *testing.T) {
-	assert := NewAssert(t)
-
-	for i := 0; i < 10000000; i++ {
-		now := TimeNow()
-		assert(TimeNow().Sub(now) < 40*time.Millisecond).IsTrue()
-		assert(TimeNow().Sub(now) > -20*time.Millisecond).IsTrue()
-	}
-
-	for i := 0; i < 10; i++ {
-		now := TimeNow()
-		time.Sleep(50 * time.Millisecond)
-		assert(TimeNow().Sub(now) < 150*time.Millisecond).IsTrue()
-		assert(TimeNow().Sub(now) > 30*time.Millisecond).IsTrue()
-	}
-}
-
-func TestTimeNowISOString(t *testing.T) {
-	assert := NewAssert(t)
-
-	for i := 0; i < 100000; i++ {
-		if now, err := time.Parse(
-			"2006-01-02T15:04:05.999Z07:00",
-			TimeNowISOString(),
-		); err == nil {
-			assert(time.Since(now) < 100*time.Millisecond).IsTrue()
-			assert(time.Since(now) > -20*time.Millisecond).IsTrue()
-		} else {
-			assert().Fail("time parse error")
-		}
-	}
-
-	for i := 0; i < 100000; i++ {
-		atomic.StorePointer(&timeNowPointer, nil)
-		if now, err := time.Parse(
-			"2006-01-02T15:04:05.999Z07:00",
-			TimeNowISOString(),
-		); err == nil {
-			assert(time.Since(now) < 40*time.Millisecond).IsTrue()
-			assert(time.Since(now) > -20*time.Millisecond).IsTrue()
-		} else {
-			assert().Fail("time parse error")
-		}
-	}
 }
 
 func TestGetRandString(t *testing.T) {
