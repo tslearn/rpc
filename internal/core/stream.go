618029e21dedcf55047cbc6e7a3297bc1dd1bd04
tslearn <tslearn@163.com>
2020-10-09 15:17:29 +0800

    fast dev

--- internal/core/stream.go
+++ internal/core/stream.go
@@ -1007,6 +1007,190 @@ func (p *Stream) writeMap(v Map, depth int) string {
 	return StreamWriteOK
 }
 
+// WriteRTArray write RTArray to stream
+func (p *Stream) WriteRTArray(v RTArray) string {
+	if thread := v.rt.lock(); thread != nil {
+		defer v.rt.unlock()
+		readStream := thread.rtStream
+
+		length := v.Size()
+		if length == -1 {
+			return "WriteRTArray: parameter is not available"
+		}
+
+		if length == 0 {
+			p.writeFrame[p.writeIndex] = 64
+			p.writeIndex++
+			if p.writeIndex == streamBlockSize {
+				p.gotoNextWriteFrame()
+			}
+			return ""
+		}
+
+		startPos := p.GetWritePos()
+
+		b := p.writeFrame[p.writeIndex:]
+		if p.writeIndex < streamBlockSize-5 {
+			p.writeIndex += 5
+		} else {
+			b = b[0:1]
+			p.SetWritePos(startPos + 5)
+		}
+
+		if length < 31 {
+			b[0] = byte(64 + length)
+		} else {
+			b[0] = 95
+		}
+
+		if length > 30 {
+			if p.writeIndex < streamBlockSize-4 {
+				l := p.writeFrame[p.writeIndex:]
+				l[0] = byte(uint32(length))
+				l[1] = byte(uint32(length) >> 8)
+				l[2] = byte(uint32(length) >> 16)
+				l[3] = byte(uint32(length) >> 24)
+				p.writeIndex += 4
+			} else {
+				p.PutBytes([]byte{
+					byte(uint32(length)),
+					byte(uint32(length) >> 8),
+					byte(uint32(length) >> 16),
+					byte(uint32(length) >> 24),
+				})
+			}
+		}
+
+		for i := 0; i < length; i++ {
+			readStream.SetReadPos(int(v.items[i].getPos()))
+			if !p.writeStreamNext(readStream) {
+				p.SetWritePos(startPos)
+				return "WriteRTArray: parameter is broken"
+			}
+		}
+
+		totalLength := uint32(p.GetWritePos() - startPos)
+		if len(b) > 1 {
+			b[1] = byte(totalLength)
+			b[2] = byte(totalLength >> 8)
+			b[3] = byte(totalLength >> 16)
+			b[4] = byte(totalLength >> 24)
+		} else {
+			endPos := p.GetWritePos()
+			p.SetWritePos(startPos + 1)
+			p.PutBytes([]byte{
+				byte(totalLength),
+				byte(totalLength >> 8),
+				byte(totalLength >> 16),
+				byte(totalLength >> 24),
+			})
+			p.SetWritePos(endPos)
+		}
+
+		return ""
+	} else {
+		return "WriteRTArray: parameter is not available"
+	}
+}
+
+// WriteRPCMap write RPCMap value to stream
+func (p *Stream) WriteRTMap(v RTMap) string {
+	if thread := v.rt.lock(); thread != nil {
+		defer v.rt.unlock()
+		readStream := thread.rtStream
+
+		length := v.Size()
+		if length == -1 {
+			return "WriteRTMap: parameter is not available"
+		}
+
+		if length == 0 {
+			p.writeFrame[p.writeIndex] = 96
+			p.writeIndex++
+			if p.writeIndex == streamBlockSize {
+				p.gotoNextWriteFrame()
+			}
+			return ""
+		}
+
+		startPos := p.GetWritePos()
+
+		b := p.writeFrame[p.writeIndex:]
+		if p.writeIndex < streamBlockSize-5 {
+			p.writeIndex += 5
+		} else {
+			b = b[0:1]
+			p.SetWritePos(startPos + 5)
+		}
+
+		if length < 31 {
+			b[0] = byte(96 + length)
+		} else {
+			b[0] = 127
+		}
+
+		if length > 30 {
+			if p.writeIndex < streamBlockSize-4 {
+				l := p.writeFrame[p.writeIndex:]
+				l[0] = byte(uint32(length))
+				l[1] = byte(uint32(length) >> 8)
+				l[2] = byte(uint32(length) >> 16)
+				l[3] = byte(uint32(length) >> 24)
+				p.writeIndex += 4
+			} else {
+				p.PutBytes([]byte{
+					byte(uint32(length)),
+					byte(uint32(length) >> 8),
+					byte(uint32(length) >> 16),
+					byte(uint32(length) >> 24),
+				})
+			}
+		}
+
+		if v.items != nil {
+			for i := 0; i < length; i++ {
+				p.WriteString(v.items[i].key)
+				readStream.SetReadPos(int(v.items[i].pos.getPos()))
+				if !p.writeStreamNext(readStream) {
+					p.SetWritePos(startPos)
+					return "WriteRTMap: parameter is broken"
+				}
+			}
+		} else if v.largeMap != nil {
+			for name, pos := range v.largeMap {
+				p.WriteString(name)
+				readStream.SetReadPos(int(pos.getPos()))
+				if !p.writeStreamNext(readStream) {
+					p.SetWritePos(startPos)
+					return "WriteRTMap: parameter is broken"
+				}
+			}
+		}
+
+		totalLength := uint32(p.GetWritePos() - startPos)
+		if len(b) > 1 {
+			b[1] = byte(totalLength)
+			b[2] = byte(totalLength >> 8)
+			b[3] = byte(totalLength >> 16)
+			b[4] = byte(totalLength >> 24)
+		} else {
+			endPos := p.GetWritePos()
+			p.SetWritePos(startPos + 1)
+			p.PutBytes([]byte{
+				byte(totalLength),
+				byte(totalLength >> 8),
+				byte(totalLength >> 16),
+				byte(totalLength >> 24),
+			})
+			p.SetWritePos(endPos)
+		}
+
+		return ""
+	} else {
+		return "WriteRTMap: parameter is not available"
+	}
+}
+
 func (p *Stream) Write(v interface{}) string {
 	return p.write(v, 64)
 }
@@ -1968,190 +2152,6 @@ func (p *Stream) ReadRTMap(rt Runtime) (RTMap, *base.Error) {
 	}
 
 	return RTMap{}, errors.ErrStreamIsBroken
-}
-
-// WriteRTArray write RTArray to stream
-func (p *Stream) WriteRTArray(v RTArray) string {
-	if thread := v.rt.lock(); thread != nil {
-		defer v.rt.unlock()
-		readStream := thread.rtStream
-
-		length := v.Size()
-		if length == -1 {
-			return "WriteRTArray: parameter is not available"
-		}
-
-		if length == 0 {
-			p.writeFrame[p.writeIndex] = 64
-			p.writeIndex++
-			if p.writeIndex == streamBlockSize {
-				p.gotoNextWriteFrame()
-			}
-			return ""
-		}
-
-		startPos := p.GetWritePos()
-
-		b := p.writeFrame[p.writeIndex:]
-		if p.writeIndex < streamBlockSize-5 {
-			p.writeIndex += 5
-		} else {
-			b = b[0:1]
-			p.SetWritePos(startPos + 5)
-		}
-
-		if length < 31 {
-			b[0] = byte(64 + length)
-		} else {
-			b[0] = 95
-		}
-
-		if length > 30 {
-			if p.writeIndex < streamBlockSize-4 {
-				l := p.writeFrame[p.writeIndex:]
-				l[0] = byte(uint32(length))
-				l[1] = byte(uint32(length) >> 8)
-				l[2] = byte(uint32(length) >> 16)
-				l[3] = byte(uint32(length) >> 24)
-				p.writeIndex += 4
-			} else {
-				p.PutBytes([]byte{
-					byte(uint32(length)),
-					byte(uint32(length) >> 8),
-					byte(uint32(length) >> 16),
-					byte(uint32(length) >> 24),
-				})
-			}
-		}
-
-		for i := 0; i < length; i++ {
-			readStream.SetReadPos(int(v.items[i].getPos()))
-			if !p.writeStreamNext(readStream) {
-				p.SetWritePos(startPos)
-				return "WriteRTArray: parameter is broken"
-			}
-		}
-
-		totalLength := uint32(p.GetWritePos() - startPos)
-		if len(b) > 1 {
-			b[1] = byte(totalLength)
-			b[2] = byte(totalLength >> 8)
-			b[3] = byte(totalLength >> 16)
-			b[4] = byte(totalLength >> 24)
-		} else {
-			endPos := p.GetWritePos()
-			p.SetWritePos(startPos + 1)
-			p.PutBytes([]byte{
-				byte(totalLength),
-				byte(totalLength >> 8),
-				byte(totalLength >> 16),
-				byte(totalLength >> 24),
-			})
-			p.SetWritePos(endPos)
-		}
-
-		return ""
-	} else {
-		return "WriteRTArray: parameter is not available"
-	}
-}
-
-// WriteRPCMap write RPCMap value to stream
-func (p *Stream) WriteRTMap(v RTMap) string {
-	if thread := v.rt.lock(); thread != nil {
-		defer v.rt.unlock()
-		readStream := thread.rtStream
-
-		length := v.Size()
-		if length == -1 {
-			return "WriteRTMap: parameter is not available"
-		}
-
-		if length == 0 {
-			p.writeFrame[p.writeIndex] = 96
-			p.writeIndex++
-			if p.writeIndex == streamBlockSize {
-				p.gotoNextWriteFrame()
-			}
-			return ""
-		}
-
-		startPos := p.GetWritePos()
-
-		b := p.writeFrame[p.writeIndex:]
-		if p.writeIndex < streamBlockSize-5 {
-			p.writeIndex += 5
-		} else {
-			b = b[0:1]
-			p.SetWritePos(startPos + 5)
-		}
-
-		if length < 31 {
-			b[0] = byte(96 + length)
-		} else {
-			b[0] = 127
-		}
-
-		if length > 30 {
-			if p.writeIndex < streamBlockSize-4 {
-				l := p.writeFrame[p.writeIndex:]
-				l[0] = byte(uint32(length))
-				l[1] = byte(uint32(length) >> 8)
-				l[2] = byte(uint32(length) >> 16)
-				l[3] = byte(uint32(length) >> 24)
-				p.writeIndex += 4
-			} else {
-				p.PutBytes([]byte{
-					byte(uint32(length)),
-					byte(uint32(length) >> 8),
-					byte(uint32(length) >> 16),
-					byte(uint32(length) >> 24),
-				})
-			}
-		}
-
-		if v.items != nil {
-			for i := 0; i < length; i++ {
-				p.WriteString(v.items[i].key)
-				readStream.SetReadPos(int(v.items[i].pos.getPos()))
-				if !p.writeStreamNext(readStream) {
-					p.SetWritePos(startPos)
-					return "WriteRTMap: parameter is broken"
-				}
-			}
-		} else if v.largeMap != nil {
-			for name, pos := range v.largeMap {
-				p.WriteString(name)
-				readStream.SetReadPos(int(pos.getPos()))
-				if !p.writeStreamNext(readStream) {
-					p.SetWritePos(startPos)
-					return "WriteRTMap: parameter is broken"
-				}
-			}
-		}
-
-		totalLength := uint32(p.GetWritePos() - startPos)
-		if len(b) > 1 {
-			b[1] = byte(totalLength)
-			b[2] = byte(totalLength >> 8)
-			b[3] = byte(totalLength >> 16)
-			b[4] = byte(totalLength >> 24)
-		} else {
-			endPos := p.GetWritePos()
-			p.SetWritePos(startPos + 1)
-			p.PutBytes([]byte{
-				byte(totalLength),
-				byte(totalLength >> 8),
-				byte(totalLength >> 16),
-				byte(totalLength >> 24),
-			})
-			p.SetWritePos(endPos)
-		}
-
-		return ""
-	} else {
-		return "WriteRTMap: parameter is not available"
-	}
 }
 
 // ReadRTValue read a RTValue value
