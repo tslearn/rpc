96340652c84599c74f3b64f2ca0dc5f20e5fbd7e
tslearn <tslearn@163.com>
2020-10-17 12:29:25 +0800

    fast dev

--- internal/core/runtime.go
+++ internal/core/runtime.go
@@ -26,82 +26,84 @@ func (p Runtime) unlock() {
 
 // OK ...
 func (p Runtime) OK(value interface{}) Return {
-	if thread := p.lock(); thread != nil {
-		defer p.unlock()
-		return thread.WriteOK(value, 1)
+	thread := p.lock()
+
+	if thread == nil {
+		base.PublishPanic(
+			errors.ErrRuntimeIllegalInCurrentGoroutine.AddDebug(base.GetFileLine(1)),
+		)
+		return emptyReturn
 	}
 
-	base.PublishPanic(
-		errors.ErrRuntimeIllegalInCurrentGoroutine.AddDebug(base.GetFileLine(1)),
-	)
-	return emptyReturn
+	defer p.unlock()
+	return thread.WriteOK(value, 1)
 }
 
 // Error ...
 func (p Runtime) Error(value error) Return {
-	if thread := p.lock(); thread != nil {
-		defer p.unlock()
-
-		if err, ok := value.(*base.Error); ok && err != nil {
-			return p.thread.WriteError(
-				err.AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
-				1,
-			)
-		} else if value != nil {
-			return p.thread.WriteError(
-				errors.ErrReplyCustom.
-					AddDebug(value.Error()).
-					AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
-				1,
-			)
-		} else {
-			return p.thread.WriteError(
-				errors.ErrRuntimeErrorArgumentIsNil.AddDebug(
-					base.AddFileLine(thread.GetExecReplyNodePath(), 1),
-				),
-				1,
-			)
-		}
+	thread := p.lock()
+	if thread == nil {
+		base.PublishPanic(
+			errors.ErrRuntimeIllegalInCurrentGoroutine.AddDebug(base.GetFileLine(1)),
+		)
+		return emptyReturn
 	}
+	defer p.unlock()
 
-	base.PublishPanic(
-		errors.ErrRuntimeIllegalInCurrentGoroutine.AddDebug(base.GetFileLine(1)),
-	)
-
-	return emptyReturn
+	if err, ok := value.(*base.Error); ok && err != nil {
+		return p.thread.WriteError(
+			err.AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
+			1,
+		)
+	} else if value != nil {
+		return p.thread.WriteError(
+			errors.ErrReplyCustom.
+				AddDebug(value.Error()).
+				AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
+			1,
+		)
+	} else {
+		return p.thread.WriteError(
+			errors.ErrRuntimeErrorArgumentIsNil.AddDebug(
+				base.AddFileLine(thread.GetExecReplyNodePath(), 1),
+			),
+			1,
+		)
+	}
 }
 
 // Call ...
 func (p Runtime) Call(target string, args ...interface{}) RTValue {
-	if thread := p.lock(); thread != nil {
-		defer p.unlock()
-		frame := thread.top
-
-		// make stream
-		stream, err := MakeRequestStream(target, frame.from, args...)
-		if err != nil {
-			return RTValue{
-				err: err.AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
-			}
+	thread := p.lock()
+	if thread == nil {
+		return RTValue{
+			err: errors.ErrRuntimeIllegalInCurrentGoroutine.
+				AddDebug(base.GetFileLine(1)),
 		}
-		defer stream.Release()
-		stream.SetDepth(frame.depth + 1)
-
-		// switch thread frame
-		thread.pushFrame()
-		defer thread.popFrame()
-
-		// eval
-		thread.Eval(stream, func(stream *Stream) {})
-
-		// return
-		return p.ParseResponseStream(stream)
 	}
+	defer p.unlock()
 
-	return RTValue{
-		err: errors.ErrRuntimeIllegalInCurrentGoroutine.
-			AddDebug(base.GetFileLine(1)),
+	frame := thread.top
+
+	// make stream
+	stream, err := MakeRequestStream(target, frame.from, args...)
+	if err != nil {
+		return RTValue{
+			err: err.AddDebug(base.AddFileLine(thread.GetExecReplyNodePath(), 1)),
+		}
 	}
+	defer stream.Release()
+	stream.SetDepth(frame.depth + 1)
+
+	// switch thread frame
+	thread.pushFrame()
+	defer thread.popFrame()
+
+	// eval
+	thread.Eval(stream, func(stream *Stream) {})
+
+	// return
+	return p.ParseResponseStream(stream)
 }
 
 // GetServiceData ...
