90086713f2d3059117d338d5aab50979f8cea65b
tslearn <tslearn@163.com>
2020-10-31 01:32:50 +0800

    fast dev

--- internal/core/zzz_bench_test.go
+++ internal/core/zzz_bench_test.go
@@ -12,7 +12,8 @@ func testWithRPCBenchmark(
 	fnCache ActionCache,
 	handler interface{},
 	serviceData Map,
-	onReady func(processor *Processor, sendBuffer []byte),
+	testCount int,
+	b *testing.B,
 	args ...interface{},
 ) bool {
 	if stream, err := MakeRequestStream("#.test:bench", "", args...); err == nil {
@@ -32,9 +33,22 @@ func testWithRPCBenchmark(
 				fileLine: "",
 				data:     serviceData,
 			}},
-			func(stream *Stream) { stream.Release() },
+			func(s *Stream) { s.Release() },
 		); err == nil {
-			onReady(processor, stream.GetBuffer())
+			sendBuffer := stream.GetBuffer()
+			runtime.GC()
+			b.ResetTimer()
+			b.ReportAllocs()
+			b.N = testCount
+			b.SetParallelism(32)
+
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					stream := NewStream()
+					stream.PutBytesTo(sendBuffer, 0)
+					processor.PutStream(stream)
+				}
+			})
 			return processor.Close()
 		}
 	}
@@ -43,155 +57,105 @@ func testWithRPCBenchmark(
 }
 
 func BenchmarkRPC_basic(b *testing.B) {
-	handler := func(rt Runtime) Return {
-		return rt.Reply(true)
-	}
-
-	onReady := func(processor *Processor, sendBuffer []byte) {
-		runtime.GC()
-		b.ResetTimer()
-		b.ReportAllocs()
-		b.N = 20000000
-		b.SetParallelism(32)
-
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				stream := NewStream()
-				stream.PutBytesTo(sendBuffer, 0)
-				processor.PutStream(stream)
-			}
-		})
-	}
-
 	testWithRPCBenchmark(
 		8192*24,
 		2048,
 		&testFuncCache{},
-		handler,
+		func(rt Runtime) Return {
+			return rt.Reply(true)
+		},
 		nil,
-		onReady,
+		20000000,
+		b,
 	)
 }
 
 func BenchmarkRPC_string(b *testing.B) {
-	handler := func(rt Runtime, rtName RTValue) Return {
-		if name, err := rtName.ToString(); err != nil {
-			panic("error")
-		} else if name != "kitty" {
-			panic("error")
-		} else {
-			return rt.Reply(true)
-		}
-	}
-
-	onReady := func(processor *Processor, sendBuffer []byte) {
-		runtime.GC()
-		b.ResetTimer()
-		b.ReportAllocs()
-		b.N = 20000000
-		b.SetParallelism(32)
-
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				stream := NewStream()
-				stream.PutBytesTo(sendBuffer, 0)
-				processor.PutStream(stream)
-			}
-		})
-	}
-
 	testWithRPCBenchmark(
 		8192*24,
 		2048,
 		&testFuncCache{},
-		handler,
+		func(rt Runtime, rtName RTValue) Return {
+			if name, err := rtName.ToString(); err != nil {
+				panic("error")
+			} else if name != "kitty" {
+				panic("error")
+			} else {
+				return rt.Reply(true)
+			}
+		},
 		nil,
-		onReady,
+		20000000,
+		b,
 		"kitty",
 	)
 }
 
 func BenchmarkRPC_array(b *testing.B) {
-	handler := func(rt Runtime, rtArray RTArray) Return {
-		if rtArray.Size() != 3 {
-			panic("error")
-		}
-		if v, err := rtArray.Get(0).ToString(); err != nil || v != "hello" {
-			panic("error")
-		}
-		if v, err := rtArray.Get(1).ToString(); err != nil || v != "world" {
-			panic("error")
-		}
-		if v, err := rtArray.Get(2).ToBool(); err != nil || v != true {
-			panic("error")
-		}
-		return rt.Reply(true)
-	}
-
-	onReady := func(processor *Processor, sendBuffer []byte) {
-		runtime.GC()
-		b.ResetTimer()
-		b.ReportAllocs()
-		b.N = 20000000
-		b.SetParallelism(32)
-
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				stream := NewStream()
-				stream.PutBytesTo(sendBuffer, 0)
-				processor.PutStream(stream)
-			}
-		})
-	}
-
 	testWithRPCBenchmark(
 		8192*24,
 		2048,
 		&testFuncCache{},
-		handler,
+		func(rt Runtime, rtArray RTArray) Return {
+			if rtArray.Size() != 3 {
+				panic("error")
+			}
+			if v, err := rtArray.Get(0).ToString(); err != nil || v != "hello" {
+				panic("error")
+			}
+			if v, err := rtArray.Get(1).ToString(); err != nil || v != "world" {
+				panic("error")
+			}
+			if v, err := rtArray.Get(2).ToBool(); err != nil || v != true {
+				panic("error")
+			}
+			return rt.Reply(true)
+		},
 		nil,
-		onReady,
+		20000000,
+		b,
 		Array{"hello", "world", true},
 	)
 }
 
 func BenchmarkRPC_map(b *testing.B) {
-	handler := func(rt Runtime, rtMap RTMap) Return {
-		if rtMap.Size() != 2 {
-			panic("error")
-		}
-		if v, err := rtMap.Get("name").ToString(); err != nil || v != "kitty" {
-			panic("error")
-		}
-		if v, err := rtMap.Get("age").ToInt64(); err != nil || v != 12 {
-			panic("error")
-		}
-		return rt.Reply(true)
-	}
-
-	onReady := func(processor *Processor, sendBuffer []byte) {
-		runtime.GC()
-		b.ResetTimer()
-		b.ReportAllocs()
-		b.N = 20000000
-		b.SetParallelism(32)
-
-		b.RunParallel(func(pb *testing.PB) {
-			for pb.Next() {
-				stream := NewStream()
-				stream.PutBytesTo(sendBuffer, 0)
-				processor.PutStream(stream)
-			}
-		})
-	}
-
 	testWithRPCBenchmark(
 		8192*24,
 		2048,
 		&testFuncCache{},
-		handler,
+		func(rt Runtime, rtMap RTMap) Return {
+			if rtMap.Size() != 2 {
+				panic("error")
+			}
+			if v, err := rtMap.Get("name").ToString(); err != nil || v != "kitty" {
+				panic("error")
+			}
+			if v, err := rtMap.Get("age").ToInt64(); err != nil || v != 12 {
+				panic("error")
+			}
+			return rt.Reply(true)
+		},
 		nil,
-		onReady,
+		20000000,
+		b,
 		Map{"name": "kitty", "age": int64(12)},
+	)
+}
+
+func BenchmarkRPC_call(b *testing.B) {
+	testWithRPCBenchmark(
+		8192*24,
+		2048,
+		&testFuncCache{},
+		func(rt Runtime, v int64) Return {
+			if v == 0 {
+				return rt.Reply(true)
+			}
+			return rt.Reply(rt.Call("#.test:bench", v-1))
+		},
+		nil,
+		5000000,
+		b,
+		int64(6),
 	)
 }
