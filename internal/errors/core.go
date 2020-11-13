17a5ca33d0f27f7ab913f5ffee76df9ce9811cbb
tslearn <tslearn@163.com>
2020-11-13 14:41:01 +0800

    fast dev

--- internal/errors/core.go
+++ internal/errors/core.go
@@ -4,220 +4,224 @@ import (
 	"github.com/rpccloud/rpc/internal/base"
 )
 
-const generalErrorSeg = 0
-
-var (
-	// ErrStream ...
-	ErrStream = base.DefineSecurityError(
-		(generalErrorSeg<<16)|1,
-		base.ErrorLevelWarn,
-		"stream error",
-	)
-
-	// ErrUnsupportedValue ...
-	ErrUnsupportedValue = base.DefineDevelopError(
-		(generalErrorSeg<<16)|2,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrServiceName ... *
-	ErrServiceName = base.DefineDevelopError(
-		(generalErrorSeg<<16)|3,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrServiceIsNil ... *
-	ErrServiceIsNil = base.DefineDevelopError(
-		(generalErrorSeg<<16)|4,
-		base.ErrorLevelFatal,
-		"service is nil",
-	)
-
-	// ErrServiceOverflow ... *
-	ErrServiceOverflow = base.DefineDevelopError(
-		(generalErrorSeg<<16)|5,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrActionName ... *
-	ErrActionName = base.DefineDevelopError(
-		(generalErrorSeg<<16)|6,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrActionHandler ... *
-	ErrActionHandler = base.DefineDevelopError(
-		(generalErrorSeg<<16)|7,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrActionCloseTimeout ... *
-	ErrActionCloseTimeout = base.DefineDevelopError(
-		(generalErrorSeg<<16)|8,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrActionCustom ...
-	ErrActionCustom = base.DefineActionError(
-		(generalErrorSeg<<16)|9,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrActionPanic ...
-	ErrActionPanic = base.DefineDevelopError(
-		(generalErrorSeg<<16)|10,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrTargetNotExist ...
-	ErrTargetNotExist = base.DefineSecurityError(
-		(generalErrorSeg<<16)|11,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrCallOverflow ...
-	ErrCallOverflow = base.DefineSecurityError(
-		(generalErrorSeg<<16)|12,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrArgumentsNotMatch ...
-	ErrArgumentsNotMatch = base.DefineSecurityError(
-		(generalErrorSeg<<16)|13,
-		base.ErrorLevelError,
-		"",
-	)
-
-	// ErrRuntimeIllegalInCurrentGoroutine ...
-	ErrRuntimeIllegalInCurrentGoroutine = base.DefineDevelopError(
-		(generalErrorSeg<<16)|14,
-		base.ErrorLevelFatal,
-		"Runtime is illegal in current goroutine",
-	)
-
-	// ErrRuntimeReplyHasBeenCalled ...
-	ErrRuntimeReplyHasBeenCalled = base.DefineDevelopError(
-		(generalErrorSeg<<16)|15,
-		base.ErrorLevelError,
-		"Runtime.Reply has been called before",
-	)
-
-	// ErrRuntimeExternalReturn ...
-	ErrRuntimeExternalReturn = base.DefineDevelopError(
-		(generalErrorSeg<<16)|16,
-		base.ErrorLevelError,
-		"action must be return through Runtime.OK or Runtime.Error",
-	)
-
-	// ErrRTArrayIndexOverflow ...
-	ErrRTArrayIndexOverflow = base.DefineDevelopError(
-		(generalErrorSeg<<16)|17,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrRTMapNameNotFound ...
-	ErrRTMapNameNotFound = base.DefineDevelopError(
-		(generalErrorSeg<<16)|18,
-		base.ErrorLevelFatal,
-		"",
-	)
-
-	// ErrNumOfThreadsIsWrong ... *
-	ErrNumOfThreadsIsWrong = base.DefineConfigError(
-		(generalErrorSeg<<16)|19,
-		base.ErrorLevelFatal,
-		"numOfThreads is wrong",
-	)
-
-	// ErrMaxNodeDepthIsWrong ... *
-	ErrMaxNodeDepthIsWrong = base.DefineConfigError(
-		(generalErrorSeg<<16)|20,
-		base.ErrorLevelFatal,
-		"maxNodeDepth is wrong",
-	)
-
-	// ErrCacheMkdirAll ... *
-	ErrCacheMkdirAll = base.DefineConfigError(
-		(generalErrorSeg<<16)|21,
-		base.ErrorLevelFatal,
-		"create directory error",
-	)
-
-	// ErrCacheWriteFile ... *
-	ErrCacheWriteFile = base.DefineConfigError(
-		(generalErrorSeg<<16)|22,
-		base.ErrorLevelFatal,
-		"write to file error",
-	)
-)
-
-const kernelErrorSeg = 1000
-
+// Kind 0 (Kernel error)
 var (
 	// ErrFnCacheIllegalKindString ... *
 	ErrFnCacheIllegalKindString = base.DefineKernelError(
-		(kernelErrorSeg<<16)|1,
+		1,
 		base.ErrorLevelFatal,
 		"",
 	)
 
 	// ErrFnCacheDuplicateKindString ... *
 	ErrFnCacheDuplicateKindString = base.DefineKernelError(
-		(kernelErrorSeg<<16)|2,
+		2,
 		base.ErrorLevelFatal,
 		"",
 	)
 
 	// ErrProcessorOnReturnStreamIsNil ... *
 	ErrProcessorOnReturnStreamIsNil = base.DefineKernelError(
-		(kernelErrorSeg<<16)|3,
+		3,
 		base.ErrorLevelFatal,
 		"onReturnStream is nil",
 	)
 
 	// ErrProcessorMaxCallDepthIsWrong ... *
 	ErrProcessorMaxCallDepthIsWrong = base.DefineKernelError(
-		(kernelErrorSeg<<16)|4,
+		4,
 		base.ErrorLevelFatal,
 		"maxCallDepth is wrong",
 	)
 
 	// ErrProcessorNodeMetaIsNil ... *
 	ErrProcessorNodeMetaIsNil = base.DefineKernelError(
-		(kernelErrorSeg<<16)|5,
+		5,
 		base.ErrorLevelFatal,
 		"node meta is nil",
 	)
 
 	// ErrProcessorActionMetaIsNil ... *
 	ErrProcessorActionMetaIsNil = base.DefineKernelError(
-		(kernelErrorSeg<<16)|6,
+		6,
 		base.ErrorLevelFatal,
 		"action meta is nil",
 	)
 
 	// ErrThreadEvalFatal ...
 	ErrThreadEvalFatal = base.DefineKernelError(
-		(kernelErrorSeg<<16)|7,
+		7,
 		base.ErrorLevelFatal,
 		"",
 	)
 
 	// ErrProcessorIsNotRunning ... *
 	ErrProcessorIsNotRunning = base.DefineKernelError(
-		(kernelErrorSeg<<16)|8,
+		8,
 		base.ErrorLevelFatal,
 		"processor is not running",
+	)
+)
+
+// Kind 1 (Caused by remote abuse or error)
+var (
+	// ErrStream ...
+	ErrStream = base.DefineSecurityError(
+		10001,
+		base.ErrorLevelWarn,
+		"stream error",
+	)
+
+	// ErrUnsupportedValue ...
+	ErrUnsupportedValue = base.DefineDevelopError(
+		10002,
+		base.ErrorLevelError,
+		"",
+	)
+
+	// ErrTargetNotExist ...
+	ErrTargetNotExist = base.DefineSecurityError(
+		10011,
+		base.ErrorLevelError,
+		"",
+	)
+
+	// ErrCallOverflow ...
+	ErrCallOverflow = base.DefineSecurityError(
+		10012,
+		base.ErrorLevelError,
+		"",
+	)
+
+	// ErrArgumentsNotMatch ...
+	ErrArgumentsNotMatch = base.DefineSecurityError(
+		10013,
+		base.ErrorLevelError,
+		"",
+	)
+)
+
+// Kind 2 (Caused by incorrect use of api)
+var (
+	// ErrServiceName ... *
+	ErrServiceName = base.DefineDevelopError(
+		10003,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrServiceIsNil ... *
+	ErrServiceIsNil = base.DefineDevelopError(
+		10004,
+		base.ErrorLevelFatal,
+		"service is nil",
+	)
+
+	// ErrServiceOverflow ... *
+	ErrServiceOverflow = base.DefineDevelopError(
+		10005,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrActionName ... *
+	ErrActionName = base.DefineDevelopError(
+		10006,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrActionHandler ... *
+	ErrActionHandler = base.DefineDevelopError(
+		10007,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrNumOfThreadsIsWrong ... *
+	ErrNumOfThreadsIsWrong = base.DefineConfigError(
+		10019,
+		base.ErrorLevelFatal,
+		"numOfThreads is wrong",
+	)
+
+	// ErrMaxNodeDepthIsWrong ... *
+	ErrMaxNodeDepthIsWrong = base.DefineConfigError(
+		10020,
+		base.ErrorLevelFatal,
+		"maxNodeDepth is wrong",
+	)
+
+	// ErrCacheMkdirAll ... *
+	ErrCacheMkdirAll = base.DefineConfigError(
+		10021,
+		base.ErrorLevelFatal,
+		"create directory error",
+	)
+
+	// ErrCacheWriteFile ... *
+	ErrCacheWriteFile = base.DefineConfigError(
+		10022,
+		base.ErrorLevelFatal,
+		"write to file error",
+	)
+)
+
+// Kind 3 (Caused by Actions)
+var (
+	// ErrActionCloseTimeout ... *
+	ErrActionCloseTimeout = base.DefineDevelopError(
+		10008,
+		base.ErrorLevelError,
+		"",
+	)
+
+	// ErrActionCustom ...
+	ErrActionCustom = base.DefineActionError(
+		10009,
+		base.ErrorLevelError,
+		"",
+	)
+
+	// ErrActionPanic ...
+	ErrActionPanic = base.DefineDevelopError(
+		10010,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrRuntimeIllegalInCurrentGoroutine ...
+	ErrRuntimeIllegalInCurrentGoroutine = base.DefineDevelopError(
+		10014,
+		base.ErrorLevelFatal,
+		"Runtime is illegal in current goroutine",
+	)
+
+	// ErrRuntimeReplyHasBeenCalled ...
+	ErrRuntimeReplyHasBeenCalled = base.DefineDevelopError(
+		10015,
+		base.ErrorLevelError,
+		"Runtime.Reply has been called before",
+	)
+
+	// ErrRuntimeExternalReturn ...
+	ErrRuntimeExternalReturn = base.DefineDevelopError(
+		10016,
+		base.ErrorLevelError,
+		"action must be return through Runtime.OK or Runtime.Error",
+	)
+
+	// ErrRTArrayIndexOverflow ...
+	ErrRTArrayIndexOverflow = base.DefineDevelopError(
+		10017,
+		base.ErrorLevelFatal,
+		"",
+	)
+
+	// ErrRTMapNameNotFound ...
+	ErrRTMapNameNotFound = base.DefineDevelopError(
+		10018,
+		base.ErrorLevelFatal,
+		"",
 	)
 )
