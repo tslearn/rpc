eb01d8ada4a86d438138f496e6bc113b8d120014
tslearn <tslearn@163.com>
2020-12-06 13:37:41 +0800

    fast dev

---
+++ internal/xadapter/tcp/client.go
@@ -0,0 +1,106 @@
+// +build linux freebsd dragonfly darwin
+
+package tcp
+
+import (
+	"github.com/rpccloud/rpc/internal/adapter"
+	"github.com/rpccloud/rpc/internal/errors"
+	"net"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+const tcpClientAdapterRunning = uint32(1)
+const tcpClientAdapterClosing = uint32(2)
+const tcpClientAdapterClosed = uint32(0)
+
+type tcpClientAdapter struct {
+	status   uint32
+	closeCH  chan bool
+	addr     string
+	conn     *adapter.EventConn
+	rBufSize int
+	wBufSize int
+	sync.Mutex
+}
+
+// NewTCPClientAdapter ...
+func NewTCPClientAdapter(
+	addr string,
+	rBufSize int,
+	wBufSize int,
+) adapter.IAdapter {
+	return &tcpClientAdapter{
+		status:   tcpClientAdapterClosed,
+		closeCH:  make(chan bool),
+		addr:     addr,
+		rBufSize: rBufSize,
+		wBufSize: wBufSize,
+	}
+}
+
+// Open ...
+func (p *tcpClientAdapter) Open(receiver adapter.XReceiver) {
+	if p.conn != nil {
+		receiver.OnEventConnError(nil, errors.ErrTCPClientAdapterAlreadyRunning)
+	} else if conn, e := net.Dial("tcp", p.addr); e != nil {
+		receiver.OnEventConnError(
+			nil,
+			errors.ErrTCPClientAdapterDail.AddDebug(e.Error()),
+		)
+	} else if fd, e := adapter.GetFD(conn); e != nil {
+		receiver.OnEventConnError(
+			nil,
+			errors.ErrTCPClientAdapterDail.AddDebug(e.Error()),
+		)
+		_ = conn.Close()
+	} else {
+		atomic.StoreUint32(&p.status, tcpClientAdapterRunning)
+		p.conn = adapter.NewEventConn(adapter.NewReceiverHook(
+			receiver,
+			nil,
+			func(eventConn *adapter.EventConn) {
+				if eventConn != nil && eventConn.GetFD() == fd {
+					go func() {
+						p.Close(receiver)
+					}()
+				}
+			},
+			nil,
+			nil,
+		), conn, fd, p.rBufSize)
+
+		manager := adapter.NewLoopManager(1, receiver)
+		manager.AllocChannel().AddConn(p.conn)
+
+		for atomic.LoadUint32(&p.status) == tcpClientAdapterRunning {
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		manager.Close()
+		p.conn = nil
+		atomic.StoreUint32(&p.status, tcpClientAdapterClosed)
+		p.closeCH <- true
+	}
+}
+
+// Close ...
+func (p *tcpClientAdapter) Close(receiver adapter.XReceiver) {
+	if atomic.CompareAndSwapUint32(
+		&p.status,
+		tcpClientAdapterRunning,
+		tcpClientAdapterClosing,
+	) {
+		if e := p.conn.Close(); e != nil {
+			receiver.OnEventConnError(
+				nil,
+				errors.ErrTCPClientAdapterClose.AddDebug(e.Error()),
+			)
+		}
+
+		<-p.closeCH
+	} else {
+		receiver.OnEventConnError(nil, errors.ErrTCPClientAdapterNotRunning)
+	}
+}
---
+++ internal/xadapter/tcp/server.go
@@ -0,0 +1,100 @@
+// +build linux freebsd dragonfly darwin
+
+package tcp
+
+import (
+	"github.com/rpccloud/rpc/internal/adapter"
+	"github.com/rpccloud/rpc/internal/errors"
+	"net"
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+const tcpServerAdapterRunning = uint32(1)
+const tcpServerAdapterClosing = uint32(2)
+const tcpServerAdapterClosed = uint32(0)
+
+type tcpServerAdapter struct {
+	status   uint32
+	closeCH  chan bool
+	addr     string
+	server   net.Listener
+	rBufSize int
+	wBufSize int
+	sync.Mutex
+}
+
+// NewTCPServerAdapter ...
+func NewTCPServerAdapter(
+	addr string,
+	rBufSize int,
+	wBufSize int,
+) adapter.IAdapter {
+	return &tcpServerAdapter{
+		status:   tcpServerAdapterClosed,
+		closeCH:  make(chan bool),
+		addr:     addr,
+		rBufSize: rBufSize,
+		wBufSize: wBufSize,
+		server:   nil,
+	}
+}
+
+// Open ...
+func (p *tcpServerAdapter) Open(receiver adapter.XReceiver) {
+	cpus := runtime.NumCPU()
+	if p.server != nil {
+		receiver.OnEventConnError(nil, errors.ErrTCPServerAdapterAlreadyRunning)
+	} else if server, e := net.Listen("tcp", p.addr); e != nil {
+		receiver.OnEventConnError(
+			nil,
+			errors.ErrTCPServerAdapterListen.AddDebug(e.Error()),
+		)
+	} else {
+		atomic.StoreUint32(&p.status, tcpServerAdapterRunning)
+		p.server = server
+		manager := adapter.NewLoopManager(cpus, receiver)
+		for atomic.LoadUint32(&p.status) == tcpServerAdapterRunning {
+			if conn, e := p.server.Accept(); e != nil {
+				receiver.OnEventConnError(
+					nil,
+					errors.ErrTCPServerAdapterAccept.AddDebug(e.Error()),
+				)
+			} else if fd, e := adapter.GetFD(conn); e != nil {
+				receiver.OnEventConnError(
+					nil,
+					errors.ErrTCPServerAdapterAccept.AddDebug(e.Error()),
+				)
+			} else {
+				manager.AllocChannel().AddConn(
+					adapter.NewEventConn(receiver, conn, fd, p.rBufSize),
+				)
+			}
+		}
+		manager.Close()
+		p.server = nil
+		atomic.StoreUint32(&p.status, tcpServerAdapterClosed)
+		p.closeCH <- true
+	}
+}
+
+// Close ...
+func (p *tcpServerAdapter) Close(receiver adapter.XReceiver) {
+	if atomic.CompareAndSwapUint32(
+		&p.status,
+		tcpServerAdapterRunning,
+		tcpServerAdapterClosing,
+	) {
+		if e := p.server.Close(); e != nil {
+			receiver.OnEventConnError(
+				nil,
+				errors.ErrTCPServerAdapterClose.AddDebug(e.Error()),
+			)
+		}
+
+		<-p.closeCH
+	} else {
+		receiver.OnEventConnError(nil, errors.ErrTCPServerAdapterNotRunning)
+	}
+}
