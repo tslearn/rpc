51042255ee840e6276c76adea4662e982b9ea157
tslearn <tslearn@163.com>
2020-12-08 02:59:26 +0800

    fast dev

---
+++ internal/adapter/sync/client_adapter.go
@@ -0,0 +1,57 @@
+package sync
+
+import (
+	"github.com/rpccloud/rpc/internal/adapter"
+	"github.com/rpccloud/rpc/internal/errors"
+	"net"
+)
+
+type ClientAdapter struct {
+	network  string
+	addr     string
+	rBufSize int
+	wBufSize int
+	receiver adapter.IReceiver
+	conn     *Conn
+}
+
+func NewAsyncServerAdapter(
+	network string,
+	addr string,
+	rBufSize int,
+	wBufSize int,
+	receiver adapter.IReceiver,
+) *adapter.RunnableService {
+	return adapter.NewRunnableService(&ClientAdapter{
+		network:  network,
+		addr:     addr,
+		rBufSize: rBufSize,
+		wBufSize: wBufSize,
+		receiver: receiver,
+	})
+}
+
+func (p *ClientAdapter) OnOpen() bool {
+	if netConn, e := net.Dial(p.network, p.addr); e != nil {
+		p.receiver.OnConnError(nil, errors.ErrTemp.AddDebug(e.Error()))
+		return false
+	} else {
+		p.conn = NewConn(netConn, p.rBufSize, p.wBufSize)
+		p.conn.SetNext(adapter.NewStreamConn(p.conn, p.receiver))
+		return true
+	}
+}
+
+func (p *ClientAdapter) OnRun(service *adapter.RunnableService) {
+	for service.IsRunning() {
+		p.conn.TriggerRead()
+	}
+}
+
+func (p *ClientAdapter) OnWillClose() {
+	p.conn.Close()
+}
+
+func (p *ClientAdapter) OnDidClose() {
+	// do nothing
+}
---
+++ internal/adapter/sync/conn.go
@@ -0,0 +1,78 @@
+package sync
+
+import (
+	"github.com/rpccloud/rpc/internal/adapter"
+	"github.com/rpccloud/rpc/internal/base"
+	"github.com/rpccloud/rpc/internal/errors"
+	"net"
+)
+
+type Conn struct {
+	netConn net.Conn
+	next    adapter.XConn
+	rBuf    []byte
+	wBuf    []byte
+}
+
+func NewConn(
+	netConn net.Conn,
+	rBufSize int,
+	wBufSize int,
+) *Conn {
+	return &Conn{
+		netConn: netConn,
+		next:    nil,
+		rBuf:    make([]byte, rBufSize),
+		wBuf:    make([]byte, wBufSize),
+	}
+}
+
+func (p *Conn) SetNext(next adapter.XConn) {
+	p.next = next
+}
+
+func (p *Conn) TriggerRead() {
+	if n, e := p.netConn.Read(p.rBuf); e != nil {
+		p.OnError(errors.ErrTemp.AddDebug(e.Error()))
+	} else {
+		p.OnReadBytes(p.rBuf[:n])
+	}
+}
+
+func (p *Conn) OnOpen() {
+	p.next.OnOpen()
+}
+
+func (p *Conn) OnClose() {
+	p.next.OnClose()
+}
+
+func (p *Conn) OnError(err *base.Error) {
+	p.next.OnError(err)
+}
+
+func (p *Conn) OnReadBytes(b []byte) {
+	p.next.OnReadBytes(b)
+}
+
+func (p *Conn) OnFillWrite(b []byte) int {
+	return p.next.OnFillWrite(b)
+}
+
+func (p *Conn) TriggerWrite() {
+	panic("not implement")
+}
+
+func (p *Conn) Close() {
+	if e := p.netConn.Close(); e != nil {
+		p.OnError(errors.ErrTemp.AddDebug(e.Error()))
+	}
+}
+
+func (p *Conn) LocalAddr() net.Addr {
+	return p.netConn.LocalAddr()
+}
+
+func (p *Conn) RemoteAddr() net.Addr {
+	return p.netConn.RemoteAddr()
+}
