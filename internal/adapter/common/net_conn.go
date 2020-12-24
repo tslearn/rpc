6d58edd15dfa3c3915692b640c2c59186bef73c9
tslearn <tslearn@163.com>
2020-12-24 17:48:03 +0800

    fast dev

---
+++ internal/adapter/common/net_conn.go
@@ -0,0 +1,137 @@
+package common
+
+import (
+	"net"
+	"sync"
+
+	"github.com/rpccloud/rpc/internal/base"
+	"github.com/rpccloud/rpc/internal/errors"
+)
+
+// NetConn ...
+type NetConn struct {
+	isRunning bool
+	netConn   net.Conn
+	next      IConn
+	rBuf      []byte
+	wBuf      []byte
+	sync.Mutex
+}
+
+// NewNetConn ...
+func NewNetConn(
+	netConn net.Conn,
+	rBufSize int,
+	wBufSize int,
+) *NetConn {
+	return &NetConn{
+		isRunning: true,
+		netConn:   netConn,
+		next:      nil,
+		rBuf:      make([]byte, rBufSize),
+		wBuf:      make([]byte, wBufSize),
+	}
+}
+
+// SetNext ...
+func (p *NetConn) SetNext(next IConn) {
+	p.next = next
+}
+
+// OnOpen ...
+func (p *NetConn) OnOpen() {
+	p.next.OnOpen()
+}
+
+// OnClose ...
+func (p *NetConn) OnClose() {
+	p.next.OnClose()
+}
+
+// OnError ...
+func (p *NetConn) OnError(err *base.Error) {
+	p.next.OnError(err)
+}
+
+// Close ...
+func (p *NetConn) Close() {
+	p.Lock()
+	defer p.Unlock()
+
+	if p.isRunning {
+		p.isRunning = false
+		if e := p.netConn.Close(); e != nil {
+			p.OnError(errors.ErrTemp.AddDebug(e.Error()))
+		}
+	}
+}
+
+// LocalAddr ...
+func (p *NetConn) LocalAddr() net.Addr {
+	return p.netConn.LocalAddr()
+}
+
+// RemoteAddr ...
+func (p *NetConn) RemoteAddr() net.Addr {
+	return p.netConn.RemoteAddr()
+}
+
+// OnReadReady ...
+func (p *NetConn) OnReadReady() bool {
+	n, e := p.netConn.Read(p.rBuf)
+	if e != nil {
+		p.OnError(errors.ErrTemp.AddDebug(e.Error()))
+		return false
+	}
+	p.next.OnReadBytes(p.rBuf[:n])
+	return true
+}
+
+// OnWriteReady ...
+func (p *NetConn) OnWriteReady() {
+	p.Lock()
+	defer p.Unlock()
+
+	isTriggerFinish := false
+
+	for !isTriggerFinish {
+		bufLen := 0
+
+		for !isTriggerFinish && bufLen < len(p.wBuf) {
+			if n := p.next.OnFillWrite(p.wBuf[bufLen:]); n > 0 {
+				bufLen += n
+			} else {
+				isTriggerFinish = true
+			}
+		}
+
+		start := 0
+		for start < bufLen {
+			if n, e := p.netConn.Write(p.wBuf[start:bufLen]); e != nil {
+				p.OnError(errors.ErrTemp.AddDebug(e.Error()))
+			} else {
+				start += n
+			}
+		}
+	}
+}
+
+// OnReadBytes ...
+func (p *NetConn) OnReadBytes(b []byte) {
+	panic("kernel error, this code should not be called")
+}
+
+// OnFillWrite ...
+func (p *NetConn) OnFillWrite(b []byte) int {
+	panic("kernel error, this code should not be called")
+}
+
+// GetFD ...
+func (p *NetConn) GetFD() int {
+	panic("kernel error, this code should not be called")
+}
+
+// SetFD ...
+func (p *NetConn) SetFD(fd int) {
+	panic("kernel error, this code should not be called")
+}
