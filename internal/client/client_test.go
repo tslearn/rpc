927e0596fdd05067b4faec69a41b42301cf21dff
tslearn <tslearn@163.com>
2021-01-01 14:52:48 +0800

    fast dev

--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -17,9 +17,11 @@ func TestClient_Debug(t *testing.T) {
 
 	time.Sleep(500 * time.Millisecond)
 	rpcClient := newClient("tcp", "0.0.0.0:28888", nil, 1200, 1200)
-	for i := 0; i < 2; i++ {
+	for i := 0; i < 1; i++ {
 		fmt.Println(rpcClient.SendMessage(8*time.Second, "#.test:SayHello", i))
 	}
+
+	time.Sleep(15 * time.Second)
 	rpcClient.Close()
 	rpcServer.Close()
 }
