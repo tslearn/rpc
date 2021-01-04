192b6a7617d46d5eda8ddcc72284995f5e11bbf5
tslearn <tslearn@163.com>
2021-01-05 02:33:44 +0800

    fast dev

--- internal/adapter/adapter_test.go
+++ internal/adapter/adapter_test.go
@@ -2,10 +2,81 @@ package adapter
 
 import (
 	"github.com/rpccloud/rpc/internal/base"
+	"github.com/rpccloud/rpc/internal/core"
 	"net"
 	"strings"
+	"sync"
 	"testing"
 )
+
+type testSingleReceiver struct {
+	streamConn *StreamConn
+	errCH      chan *base.Error
+	streamCH   chan *core.Stream
+	sync.Mutex
+}
+
+func newTestSingleReceiver() *testSingleReceiver {
+	return &testSingleReceiver{
+		streamConn: nil,
+		errCH:      make(chan *base.Error),
+		streamCH:   make(chan *core.Stream),
+	}
+}
+
+func (p *testSingleReceiver) OnConnOpen(streamConn *StreamConn) {
+	p.Lock()
+	defer p.Unlock()
+	if p.streamConn == nil {
+		p.streamConn = streamConn
+	} else {
+		panic("error")
+	}
+}
+
+func (p *testSingleReceiver) OnConnClose(streamConn *StreamConn) {
+	p.Lock()
+	defer p.Unlock()
+	if p.streamConn != nil && p.streamConn == streamConn {
+		p.streamConn = nil
+	} else {
+		panic("error")
+	}
+}
+
+func (p *testSingleReceiver) OnConnReadStream(
+	streamConn *StreamConn,
+	stream *core.Stream,
+) {
+	p.Lock()
+	defer p.Unlock()
+	if p.streamConn != nil && p.streamConn == streamConn {
+		p.streamCH <- stream
+	} else {
+		panic("error")
+	}
+}
+
+func (p *testSingleReceiver) OnConnError(
+	streamConn *StreamConn,
+	err *base.Error,
+) {
+	p.Lock()
+	defer p.Unlock()
+	if p.streamConn != nil && p.streamConn == streamConn {
+		p.errCH <- err
+	} else {
+		panic("error")
+	}
+}
+
+func (p *testSingleReceiver) GetStream() *core.Stream {
+	return <-p.streamCH
+}
+
+func (p *testSingleReceiver) GetError() *base.Error {
+	return <-p.errCH
+}
 
 func TestAdapter(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
--- internal/adapter/service_test.go
+++ internal/adapter/service_test.go
@@ -1,9 +1,45 @@
 package adapter
 
-import "testing"
+import (
+	"github.com/rpccloud/rpc/internal/base"
+	"testing"
+)
 
 func TestSyncTCPServerService_Open(t *testing.T) {
+	t.Run("test tcp", func(t *testing.T) {
+		assert := base.NewAssert(t)
+		receiver := newTestSingleReceiver()
+		v := &syncTCPServerService{
+			adapter: NewClientAdapter(
+				"tcp", "0.0.0.0:65432", nil, 1200, 1200, receiver,
+			),
+			ln:         nil,
+			orcManager: base.NewORCManager(),
+		}
+		assert(v.Open()).IsTrue()
+		assert(v.ln).IsNotNil()
+	})
 
+	t.Run("test tls", func(t *testing.T) {
+		assert := base.NewAssert(t)
+		receiver := newTestSingleReceiver()
+		tlsConfig, e := base.GetTLSServerConfig(
+			"../cert/server.pem",
+			"../cert/server-key.pem",
+		)
+		if e != nil {
+			panic(e)
+		}
+		v := &syncTCPServerService{
+			adapter: NewClientAdapter(
+				"tcp", "0.0.0.0:65432", tlsConfig, 1200, 1200, receiver,
+			),
+			ln:         nil,
+			orcManager: base.NewORCManager(),
+		}
+		assert(v.Open()).IsTrue()
+		assert(v.ln).IsNotNil()
+	})
 }
 
 func TestSyncTCPServerService_Run(t *testing.T) {
