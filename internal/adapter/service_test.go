c3a48f7744d52f71d05b0378f42fba8d4c6af32a
tslearn <tslearn@163.com>
2021-01-05 22:15:27 +0800

    fast dev

--- internal/adapter/service_test.go
+++ internal/adapter/service_test.go
@@ -1,8 +1,8 @@
 package adapter
 
 import (
-	"fmt"
 	"github.com/rpccloud/rpc/internal/base"
+	"github.com/rpccloud/rpc/internal/errors"
 	"net"
 	"path"
 	"reflect"
@@ -74,11 +74,19 @@ func TestSyncTCPServerService_Open(t *testing.T) {
 	})
 }
 
-func TestSyncTCPServerService_Run(t *testing.T) {
-
-}
+//func TestSyncTCPServerService_Run(t *testing.T) {
+//    _, curFile, _, _ := runtime.Caller(0)
+//    curDir := path.Dir(curFile)
+//
+//    t.Run("test tcp", func(t *testing.T) {
+//
+//    })
+//}
 
 func TestSyncTCPServerService_Close(t *testing.T) {
+	_, curFile, _, _ := runtime.Caller(0)
+	curDir := path.Dir(curFile)
+
 	t.Run("close error", func(t *testing.T) {
 		assert := base.NewAssert(t)
 		receiver := newTestSingleReceiver()
@@ -90,14 +98,16 @@ func TestSyncTCPServerService_Close(t *testing.T) {
 			orcManager: base.NewORCManager(),
 		}
 		assert(v.Open()).IsTrue()
-		*(*unsafe.Pointer)(getFieldPointer(
-			v.ln.(*net.TCPListener),
-			"fd",
-		)) = nil
-		assert(v.Close()).IsTrue()
-
-		fmt.Println(receiver.GetError())
-
+		tcpLn := v.ln.(*net.TCPListener)
+		fdPtr := (*unsafe.Pointer)(getFieldPointer(tcpLn, "fd"))
+		originFD := *fdPtr
+		*fdPtr = nil
+		v.Close()
+		assert(receiver.GetError()).Equal(
+			errors.ErrSyncTCPServerServiceClose.AddDebug("invalid argument"),
+		)
+		*fdPtr = originFD
+		_ = tcpLn.Close()
 	})
 
 	t.Run("test tcp", func(t *testing.T) {
@@ -111,7 +121,29 @@ func TestSyncTCPServerService_Close(t *testing.T) {
 			orcManager: base.NewORCManager(),
 		}
 		assert(v.Open()).IsTrue()
-		assert(v.Close()).IsTrue()
+		v.Close()
+		assert(receiver.GetError()).IsNil()
+	})
 
+	t.Run("test tls", func(t *testing.T) {
+		assert := base.NewAssert(t)
+		receiver := newTestSingleReceiver()
+		tlsConfig, e := base.GetTLSServerConfig(
+			path.Join(curDir, "_cert_", "server.crt"),
+			path.Join(curDir, "_cert_", "server.key"),
+		)
+		if e != nil {
+			panic(e)
+		}
+		v := &syncTCPServerService{
+			adapter: NewClientAdapter(
+				"tcp", "0.0.0.0:65432", tlsConfig, 1200, 1200, receiver,
+			),
+			ln:         nil,
+			orcManager: base.NewORCManager(),
+		}
+		assert(v.Open()).IsTrue()
+		v.Close()
+		assert(receiver.GetError()).IsNil()
 	})
 }
