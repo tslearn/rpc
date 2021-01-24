ce0c36ac8e476fed8ebff64d2178a09ad4ed5362
tslearn <tslearn@163.com>
2021-01-24 13:58:08 +0800

    fast dev

--- internal/gateway/config.go
+++ internal/gateway/config.go
@@ -29,7 +29,7 @@ func GetDefaultConfig() *Config {
 		heartbeatTimeout: 8 * time.Second,
 
 		serverMaxSessions:     10240000,
-		serverSessionTimeout:  60 * time.Second,
+		serverSessionTimeout:  120 * time.Second,
 		serverReadBufferSize:  1200,
 		serverWriteBufferSize: 1200,
 		serverCacheTimeout:    10 * time.Second,
---
+++ internal/gateway/config_test.go
@@ -0,0 +1,24 @@
+package gateway
+
+import (
+	"github.com/rpccloud/rpc/internal/base"
+	"testing"
+	"time"
+)
+
+func TestGetDefaultConfig(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		assert := base.NewAssert(t)
+		cfg := GetDefaultConfig()
+		assert(cfg.numOfChannels).Equal(48)
+		assert(cfg.transLimit).Equal(4 * 1024 * 1024)
+		assert(cfg.heartbeat).Equal(5 * time.Second)
+		assert(cfg.heartbeatTimeout).Equal(8 * time.Second)
+		assert(cfg.serverMaxSessions).Equal(10240000)
+		assert(cfg.serverSessionTimeout).Equal(120 * time.Second)
+		assert(cfg.serverReadBufferSize).Equal(1200)
+		assert(cfg.serverWriteBufferSize).Equal(1200)
+		assert(cfg.serverCacheTimeout).Equal(10 * time.Second)
+		assert(cfg.clientRequestInterval).Equal(3 * time.Second)
+	})
+}
