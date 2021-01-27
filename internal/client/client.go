9bd857700edd39a7ecba7dc16ae5f0dec92e8bd0
tslearn <tslearn@163.com>
2021-01-28 04:09:48 +0800

    fast dev

--- internal/client/client.go
+++ internal/client/client.go
@@ -94,122 +94,8 @@ func newClient(
 	return ret
 }
 
-// Close ...
-func (p *Client) Close() bool {
-	return p.orcManager.Close(func() bool {
-		return p.adapter.Close()
-	}, func() {
-		p.adapter = nil
-	})
-}
-
 func (p *Client) onError(err *base.Error) {
 	fmt.Println("client onError: ", err)
-}
-
-// OnConnOpen ...
-func (p *Client) OnConnOpen(streamConn *adapter.StreamConn) {
-	p.Lock()
-	defer p.Unlock()
-
-	stream := core.NewStream()
-	stream.SetCallbackID(0)
-	stream.WriteInt64(core.ControlStreamConnectRequest)
-	stream.WriteString(p.sessionString)
-	streamConn.WriteStreamAndRelease(stream)
-}
-
-// OnConnClose ...
-func (p *Client) OnConnClose(_ *adapter.StreamConn) {
-	p.Lock()
-	defer p.Unlock()
-	p.conn = nil
-}
-
-// OnConnReadStream ...
-func (p *Client) OnConnReadStream(
-	streamConn *adapter.StreamConn,
-	stream *core.Stream,
-) {
-	p.Lock()
-	defer p.Unlock()
-
-	callbackID := stream.GetCallbackID()
-
-	if p.conn == nil {
-		p.conn = streamConn
-
-		if callbackID != 0 {
-			p.OnConnError(streamConn, errors.ErrStream)
-		} else if kind, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if kind != core.ControlStreamConnectResponse {
-			p.OnConnError(streamConn, errors.ErrStream)
-		} else if sessionString, err := stream.ReadString(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if numOfChannels, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if transLimit, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if heartbeat, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if heartbeatTimeout, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if requestInterval, err := stream.ReadInt64(); err != nil {
-			p.OnConnError(streamConn, err)
-		} else if !stream.IsReadFinish() {
-			p.OnConnError(streamConn, errors.ErrStream)
-		} else {
-			if sessionString != p.sessionString {
-				// new session
-				p.sessionString = sessionString
-
-				// update config
-				p.config.numOfChannels = int(numOfChannels)
-				p.config.transLimit = int(transLimit)
-				p.config.heartbeat = time.Duration(heartbeat)
-				p.config.heartbeatTimeout = time.Duration(heartbeatTimeout)
-				p.config.requestInterval = time.Duration(requestInterval)
-
-				p.channels = make([]Channel, numOfChannels)
-				for i := 0; i < len(p.channels); i++ {
-					(&p.channels[i]).sequence = uint64(i)
-					(&p.channels[i]).item = nil
-				}
-			} else {
-				// config and channels have already initialized. so ignore this
-			}
-
-			p.lastPingTimeNS = base.TimeNow().UnixNano()
-		}
-
-		stream.Release()
-	} else {
-		if callbackID == 0 {
-			if kind, err := stream.ReadInt64(); err != nil {
-				p.OnConnError(streamConn, err)
-			} else if kind != core.ControlStreamPong {
-				p.OnConnError(streamConn, errors.ErrStream)
-			} else {
-				// ignore
-			}
-			stream.Release()
-		} else if p.channels != nil {
-			channel := &p.channels[callbackID%uint64(len(p.channels))]
-			channel.Free(stream)
-		} else {
-			// ignore
-			stream.Release()
-		}
-	}
-}
-
-// OnConnError ...
-func (p *Client) OnConnError(streamConn *adapter.StreamConn, err *base.Error) {
-	p.onError(err)
-	if streamConn != nil {
-		streamConn.Close()
-	}
 }
 
 func (p *Client) tryToSendPing(nowNS int64) {
@@ -346,4 +232,118 @@ func (p *Client) SendMessage(
 	defer retStream.Release()
 
 	return core.ParseResponseStream(retStream)
+}
+
+// Close ...
+func (p *Client) Close() bool {
+	return p.orcManager.Close(func() bool {
+		return p.adapter.Close()
+	}, func() {
+		p.adapter = nil
+	})
+}
+
+// OnConnOpen ...
+func (p *Client) OnConnOpen(streamConn *adapter.StreamConn) {
+	p.Lock()
+	defer p.Unlock()
+
+	stream := core.NewStream()
+	stream.SetCallbackID(0)
+	stream.WriteInt64(core.ControlStreamConnectRequest)
+	stream.WriteString(p.sessionString)
+	streamConn.WriteStreamAndRelease(stream)
+}
+
+// OnConnReadStream ...
+func (p *Client) OnConnReadStream(
+	streamConn *adapter.StreamConn,
+	stream *core.Stream,
+) {
+	p.Lock()
+	defer p.Unlock()
+
+	callbackID := stream.GetCallbackID()
+
+	if p.conn == nil {
+		p.conn = streamConn
+
+		if callbackID != 0 {
+			p.OnConnError(streamConn, errors.ErrStream)
+		} else if kind, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if kind != core.ControlStreamConnectResponse {
+			p.OnConnError(streamConn, errors.ErrStream)
+		} else if sessionString, err := stream.ReadString(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if numOfChannels, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if transLimit, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if heartbeat, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if heartbeatTimeout, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if requestInterval, err := stream.ReadInt64(); err != nil {
+			p.OnConnError(streamConn, err)
+		} else if !stream.IsReadFinish() {
+			p.OnConnError(streamConn, errors.ErrStream)
+		} else {
+			if sessionString != p.sessionString {
+				// new session
+				p.sessionString = sessionString
+
+				// update config
+				p.config.numOfChannels = int(numOfChannels)
+				p.config.transLimit = int(transLimit)
+				p.config.heartbeat = time.Duration(heartbeat)
+				p.config.heartbeatTimeout = time.Duration(heartbeatTimeout)
+				p.config.requestInterval = time.Duration(requestInterval)
+
+				p.channels = make([]Channel, numOfChannels)
+				for i := 0; i < len(p.channels); i++ {
+					(&p.channels[i]).sequence = uint64(i)
+					(&p.channels[i]).item = nil
+				}
+			} else {
+				// config and channels have already initialized. so ignore this
+			}
+
+			p.lastPingTimeNS = base.TimeNow().UnixNano()
+		}
+
+		stream.Release()
+	} else {
+		if callbackID == 0 {
+			if kind, err := stream.ReadInt64(); err != nil {
+				p.OnConnError(streamConn, err)
+			} else if kind != core.ControlStreamPong {
+				p.OnConnError(streamConn, errors.ErrStream)
+			} else {
+				// ignore
+			}
+			stream.Release()
+		} else if p.channels != nil {
+			channel := &p.channels[callbackID%uint64(len(p.channels))]
+			channel.Free(stream)
+		} else {
+			// ignore
+			stream.Release()
+		}
+	}
+}
+
+// OnConnError ...
+func (p *Client) OnConnError(streamConn *adapter.StreamConn, err *base.Error) {
+	p.onError(err)
+	if streamConn != nil {
+		streamConn.Close()
+	}
+}
+
+// OnConnClose ...
+func (p *Client) OnConnClose(_ *adapter.StreamConn) {
+	p.Lock()
+	defer p.Unlock()
+	p.conn = nil
 }
