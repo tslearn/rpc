9d643a6362ccd8b5af9ca55516ddf6ae56c38bde
tslearn <tslearn@163.com>
2021-03-02 16:35:34 +0800

    fast dev

--- lab/server/main.go
+++ lab/server/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/rpccloud/rpc"
-	"time"
 )
 
 func main() {
-	fmt.Println("Starting ....")
-
 	userService := rpc.NewService().
 		On("SayHello", func(rt rpc.Runtime, name rpc.String) rpc.Return {
 			return rt.Reply("Hello, " + name)
 		})
 
-	go runClient()
-
 	rpc.NewServer().
 		Listen("ws", "127.0.0.1:8888", nil).
 		AddService("user", userService, nil).
 		Open()
-}
-
-func runClient() {
-	time.Sleep(3 * time.Second)
-	client := rpc.Dial("ws", "127.0.0.1:8888")
-	fmt.Println(client.Send(3*time.Second, "#.user:SayHello", "world"))
-	client.Close()
 }
