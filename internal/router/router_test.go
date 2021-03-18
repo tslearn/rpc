cc8505518fe40d66995baa0202f8d77f3fc7aa8a
tslearn <tslearn@163.com>
2021-03-18 17:47:43 +0800

    fast dev

--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -9,12 +9,6 @@ func TestDebug(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
 		mp := make(map[string]int)
 		mp["hi"] = 3
-
 		fmt.Println(mp)
-		//ln1, e := net.Listen("tcp", "127.0.0.1:50000")
-		//fmt.Println(ln1, e)
-		//_ = ln1.Close()
-		//ln2, e := net.Listen("tcp", "127.0.0.1:50000")
-		//fmt.Println(ln2, e)
 	})
 }
